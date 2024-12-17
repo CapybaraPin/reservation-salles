5c0c51fca6e5c4636726bfc7e24c11e4bb11a35f
Hugo Robles <hugo@robles.com>
2024-12-17 11:46:04 +0100

    Mise à jour de la barre de navigation
    Adaptation de la barre de navigation pour que en fonction de ou je suis, il mettent le btn en active

--- views/elements/header.php
+++ views/elements/header.php
@@ -1,3 +1,9 @@
+<?php
+
+$page_actuelle = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+?>
+
 <!-- Section en-tête -->
 <div class="header">
     <div class="row">
@@ -13,26 +19,26 @@
             <!-- Contenu du menu pour les grands écrans -->
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-lg-0">
-                    <li class="btn-nav me-1 button-nav mb-1 mb-lg-0 btn-nav-active">
-                        <a href="/accueil">Tableau de bord</a>
+                    <li class="btn-nav me-1 button-nav mb-1 mb-lg-0 <?= ($page_actuelle == '/') ? 'btn-nav-active' : ''; ?>">
+                        <a href="/">Tableau de bord</a>
                     </li>
-                    <li class="btn-nav me-1 button-nav mb-1 mb-lg-0">
+                    <li class="btn-nav me-1 button-nav mb-1 mb-lg-0 <?= ($page_actuelle == '/reservations') ? 'btn-nav-active' : ''; ?>">
                         <a href="/reservations">Réservations</a>
                     </li>
-                    <li class="btn-nav me-1 button-nav mb-1 mb-lg-0">
+                    <li class="btn-nav me-1 button-nav mb-1 mb-lg-0 <?= ($page_actuelle == '/salles') ? 'btn-nav-active' : ''; ?>">
                         <a href="/salles">Salles</a>
                     </li>
-                    <li class="btn-nav me-1 button-nav mb-1 mb-lg-0">
+                    <li class="btn-nav me-1 button-nav mb-1 mb-lg-0 <?= ($page_actuelle == '/employes') ? 'btn-nav-active' : ''; ?>">
                         <a href="/employes">Employés</a>
                     </li>
-                    <li class="btn-nav me-1 button-nav mb-1 mb-lg-0">
+                    <li class="btn-nav me-1 button-nav mb-1 mb-lg-0 <?= ($page_actuelle == '/activites') ? 'btn-nav-active' : ''; ?>">
                         <a href="/activites">Activités</a>
                     </li>
                 </ul>
                 <!-- Bouton de déconnexion -->
                 <form method="post" action="">
                     <button name="deconnexion" class="btn btn-outline-dark me-1 me-4 ms-1 mb-1 mb-lg-0">
-                        <i class="fa-solid fa-arrow-right-from-bracket"></i> Deconnexion
+                        <i class="fa-solid fa-arrow-right-from-bracket"></i> Déconnexion
                     </button>
                 </form>
             </div>
