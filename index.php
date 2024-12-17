6c59a0fc090f03ddfd425a9a4532a2d30fe63a7e
Hugo Robles <hugo@robles.com>
2024-12-17 10:15:55 +0100

    Mise à jour du routeur
    Ajout des liens vers les contrôleurs pour les pages de salles, réservations et employés

--- index.php
+++ index.php
@@ -24,10 +24,16 @@ require __DIR__ . '/services/Auth.php';
 // Récupération des classes des contrôleurs
 require __DIR__ . '/controllers/AuthController.php';
 require __DIR__ . '/controllers/AccueilController.php';
+require __DIR__ . '/controllers/SallesController.php';
+require __DIR__ . '/controllers/ReservationsController.php';
+require __DIR__ . '/controllers/EmployesController.php';
 
 // Import des classes
 use controllers\AuthController;
 use controllers\AccueilController;
+use controllers\SallesController;
+use controllers\ReservationsController;
+use controllers\EmployesController;
 use services\Database;
 
 // Création d'une instance de Router
@@ -36,6 +42,7 @@ $router = new \Bramus\Router\Router();
 // Connexion à la base de données
 try {
     $db = new Database();
+    $pdo = $db->getPDO();
 } catch (PDOException $e) {
     die("Erreur de connexion à la base de données : " . $e->getMessage());
 }
@@ -46,7 +53,22 @@ session_start();
 $router->get('/auth', [new AuthController(), 'get']);
 $router->post('/auth', [new AuthController(), 'post']);
 
+// Définition des routes pour l'accueil
 $router->get('/', [new AccueilController(), 'get']);
+$router->post('/', [new AccueilController(), 'post']);
+
+// Défintion des routes pour les salles
+$router->get('/salles', [new SallesController(), 'get']);
+$router->post('/salles', [new SallesController(), 'post']);
+
+// Définition des routes pour les réservations
+$router->get('/reservations', [new ReservationsController(), 'get']);
+$router->post('/reservations', [new ReservationsController(), 'post']);
+
+// Définition des routes pour les employés
+$router->get('/employes', [new EmployesController(), 'get']);
+$router->post('/employes', [new EmployesController(), 'post']);
+
 
 // Démarrage du routeur
 $router->run();
