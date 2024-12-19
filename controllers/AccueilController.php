23bd9525575696b2a5e4f035a2556015092f712b
Jules Vialas <jules.vialas@gmail.com>
2024-12-19 08:34:37 +0100

    UPDATE accueil.php
    Rajout du tableau de mes réservations

--- controllers/AccueilController.php
+++ controllers/AccueilController.php
@@ -15,6 +15,20 @@ class AccueilController extends Controller
 {
     public function get()
     {
+        global $db;
+
+        // Récupération du nombre total de réservations
+        $nbReservations = $db->getNbReservations();
+
+        // Récupération de la page courante
+        $page = isset($_GET['page']) ? htmlspecialchars($_GET['page']) : 1;
+        $page = max(intval($page), 1);
+        $pageMax = ceil($nbReservations / $db::NB_LIGNES);
+        $page = min($page, $pageMax);
+
+        // Récupération des réservations
+        $reservations = $db->getReservations(($page - 1) * $db::NB_LIGNES);
+        $nbLignesPage = $db::NB_LIGNES;
         require __DIR__ . '/../views/accueil.php';
     }
 
