829a0d8c5ab1b678fbfaedf2d8650932d86d9dab
Hugo Robles <hugo@robles.com>
2025-01-08 15:38:18 +0100

    Mise à jour de la sauvegarde de la BDD

--- backupdb.sh
+++ backupdb.sh
@@ -2,7 +2,9 @@
 
 # Charger les variables d'environnement depuis le fichier .env
 if [ -f .env ]; then
-  export $(cat .env | grep -v '^#' | xargs)
+  set -a
+  . .env  # Utilisation du point pour source
+  set +a
 else
   echo "Erreur : Le fichier .env est manquant."
   exit 1
@@ -11,13 +13,23 @@ fi
 # Nom du fichier de sauvegarde
 BACKUP_FILENAME="backup-$(date +'%d-%m-%Y').sql"
 
+# Tester la connexion MySQL
+echo "Test de connexion à MySQL..."
+mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_NAME -e "SHOW TABLES;" > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "Erreur : Impossible de se connecter à la base de données MySQL."
+  exit 1
+fi
+
 # Commande de sauvegarde de la base de données
+echo "Sauvegarde de la base de données..."
 mysqldump -h$DB_HOST -u$DB_USER -p$DB_PASSWORD $DB_NAME > $BACKUP_FILENAME
 
 # Connexion au serveur FTP et transfert du fichier
 ftp -n $FTP_SERVER <<END_SCRIPT
 quote USER $FTP_USERNAME
 quote PASS $FTP_PASSWORD
+passive  # Passer en mode passif pour éviter les erreurs de connexion de données
 cd $FTP_UPLOAD_DIR
 put $BACKUP_FILENAME
 quit
